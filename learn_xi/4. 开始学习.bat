e61b06e32f06b1f341119759de7ae36c3d8aea00
zhiyong <silent.yangrun@gmail.com>
2019-04-01 09:28:18 +0800

    Update 4. 开始学习.bat
    修改错误。

--- learn_xi/4. 开始学习.bat
+++ learn_xi/4. 开始学习.bat
@@ -3,7 +3,7 @@ echo %time%
 @echo off
 set MY_PATH=d:\learn_xi\python\
 set ENV_PATH=%PATH%;%MY_PATH%
-d:\learn_xi\python\python\python d:\learn_xi\XueXiQiangGuo\interface_test.py
+d:\learn_xi\python\python d:\learn_xi\XueXiQiangGuo\interface_test.py
 echo %date%
 echo %time%
 
